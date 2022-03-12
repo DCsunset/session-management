85e70837b49721ddf01fe2ac009999a0e768c43a
Hongyi Shi <h235shi@uwaterloo.ca>
2022-03-12 18:09:28 -0500

    version1

--- session_management/src/main.rs
+++ session_management/src/main.rs
@@ -1,4 +1,50 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use rand::{thread_rng, Rng};
+use std::sync::Mutex;
+use rand::distributions::Alphanumeric;
+
+pub struct TokenServer {
+    tokens: Mutex<Vec<String>>
+}
+
+impl TokenServer {
+    pub fn new() -> TokenServer {
+        TokenServer {
+            tokens: Mutex::new(Vec::new())
+        }
+    }
+
+    pub fn token_generator(&mut self) -> String {
+
+        let rand_token: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(30)
+            .map(char::from)
+            .collect();
+
+        let token_copy = rand_token.clone();
+        self.tokens.get_mut().unwrap().push(rand_token);
+
+        return token_copy;
+    }
+
+    pub fn get_token(&mut self) {
+        let token = self.token_generator();
+    }
+
+    pub fn hello() -> impl Responder {
+        HttpResponse::Ok().body("Hello world!")
+    }
+
+    pub fn echo(req_body: String) -> impl Responder {
+        HttpResponse::Ok().body(req_body)
+    }
+
+    pub async fn manual_hello(&self) -> impl Responder {
+        HttpResponse::Ok().body("Hey there!")
+    }
+}
+
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -7,22 +53,62 @@ async fn hello() -> impl Responder {
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+    let rand_token: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+    HttpResponse::Ok().body(rand_token)
 }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+#[post("/login")]
+async fn login(mut data: web::Data<TokenServer>) -> String {
+    let rand_token: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .map(char::from)
+        .collect();
+
+    let mut tokens_copy = data.tokens.lock().unwrap();
+    let rand_token_copy = rand_token.clone();
+    tokens_copy.push(rand_token);
+    format!("Hello {}!", rand_token_copy)
+}
+
+#[post("/verify")]
+async fn verify(req_body: String, mut data: web::Data<TokenServer>) -> String {
+    let mut tokens_copy = data.tokens.lock().unwrap();
+
+    if ((*tokens_copy).contains(&req_body)) {
+        format!("Success!")
+    }
+    else {
+        format!("Failed!")
+    }
+
+}
+
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    let server = web::Data::new(TokenServer {
+        tokens: Mutex::new(vec![]),
+    });
+
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(server.clone())
             .service(hello)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(login)
+            .service(verify)
     })
     .bind("127.0.0.1:8080")?
     .run()
