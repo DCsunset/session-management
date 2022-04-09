e54eb13354175c4358af82adc4c6706dfac6e99c
DCsunset <DCsunset@users.noreply.github.com>
2022-04-09 16:22:06 -0400

    fix: fix redis client implementation

--- session_management/src/session_redis_cluster.rs
+++ session_management/src/session_redis_cluster.rs
@@ -1,35 +1,26 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
-use redis::cluster::ClusterClient;
+use redis::Client;
 use redis::Commands;
 use redis::RedisResult;
 use std::sync::Mutex;
 
 pub struct TokenServer {
-	nodes: Mutex<Vec<String>>,
-	client: Mutex<redis::cluster::ClusterClient>,
+	client: Mutex<redis::Client>,
+	conn: Mutex<redis::Connection>
 }
 
 impl TokenServer {
-	pub fn new(&mut self) -> TokenServer {
+	pub fn new(client: redis::Client) -> TokenServer {
+		let conn = client.get_connection().unwrap();
 		TokenServer {
-			nodes: Mutex::new(vec![
-				"redis://127.0.0.1:7000/".to_string(),
-				"redis://127.0.0.1:7001/".to_string(),
-				"redis://127.0.0.1:7002/".to_string(),
-				"redis://127.0.0.1:7003/".to_string(),
-				"redis://127.0.0.1:7004/".to_string(),
-				"redis://127.0.0.1:7005/".to_string(),
-			]),
-			client: Mutex::new(
-				redis::cluster::ClusterClient::open(self.nodes.get_mut().unwrap().to_vec())
-					.unwrap(),
-			),
+			client: Mutex::new(client),
+			conn: Mutex::new(conn)
 		}
 	}
 
-	pub fn token_generator(&mut self) -> String {
+	pub fn generate_token(&self) -> String {
 		let rand_token: String = thread_rng()
 			.sample_iter(&Alphanumeric)
 			.take(30)
@@ -39,14 +30,19 @@ impl TokenServer {
 		let token_copy = rand_token.clone();
 		let tolen_return = token_copy.clone();
 
-		let mut connection = self.client.get_mut().unwrap().get_connection().unwrap();
-		let _: () = connection.set(token_copy, "0").unwrap();
+		let mut conn = self.conn.lock().unwrap();
+		let _: () = conn.set(token_copy, "0").unwrap();
 
 		return tolen_return;
 	}
 
-	pub fn get_token(&mut self) {
-		let token = self.token_generator();
+	pub fn verify_token(&self, token: String) -> bool {
+		let mut conn = self.conn.lock().unwrap();
+		let rv: RedisResult<String> = conn.get(token);
+		match rv {
+			Ok(_) => true,
+			Err(_) => false
+		}
 	}
 }
 
@@ -57,28 +53,14 @@ async fn hello() -> impl Responder {
 
 #[post("/login")]
 async fn login(mut data: web::Data<TokenServer>) -> String {
-	let rand_token: String = thread_rng()
-		.sample_iter(&Alphanumeric)
-		.take(30)
-		.map(char::from)
-		.collect();
-
-	let mut client_copy = data.client.lock().unwrap();
-	let token_copy = rand_token.clone();
-	let token_print = token_copy.clone();
-	let mut connection = client_copy.get_connection().unwrap();
-	let _: () = connection.set(token_copy, "exist").unwrap();
-
-	format!("Hello {}!", token_print)
+	let token = data.generate_token();
+	format!("Hello {}!", token)
 }
 
 #[post("/verify")]
 async fn verify(req_body: String, mut data: web::Data<TokenServer>) -> String {
-	let mut client_copy = data.client.lock().unwrap();
-	let mut connection = client_copy.get_connection().unwrap();
-	let rv: RedisResult<String> = connection.get(req_body);
-
-	if let Ok(_) = rv {
+	let result = data.verify_token(req_body);
+	if result {
 		"successs".to_string()
 	} else {
 		"failure".to_string()
@@ -87,26 +69,9 @@ async fn verify(req_body: String, mut data: web::Data<TokenServer>) -> String {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-	let original_nodes = vec![
-		"redis://127.0.0.1:7000/",
-		"redis://127.0.0.1:7001/",
-		"redis://127.0.0.1:7002/",
-		"redis://127.0.0.1:7003/",
-		"redis://127.0.0.1:7004/",
-		"redis://127.0.0.1:7005/",
-	];
+	let client = redis::Client::open( "redis://127.0.0.1:4902/").unwrap();
 
-	let server = web::Data::new(TokenServer {
-		nodes: Mutex::new(vec![
-			"redis://127.0.0.1:7000/".to_string(),
-			"redis://127.0.0.1:7001/".to_string(),
-			"redis://127.0.0.1:7002/".to_string(),
-			"redis://127.0.0.1:7003/".to_string(),
-			"redis://127.0.0.1:7004/".to_string(),
-			"redis://127.0.0.1:7005/".to_string(),
-		]),
-		client: Mutex::new(redis::cluster::ClusterClient::open(original_nodes).unwrap()),
-	});
+	let server = web::Data::new(TokenServer::new(client));
 
 	HttpServer::new(move || {
 		App::new()
